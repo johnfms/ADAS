bf70024bbf4dc9ae6fd19e1d6d5c5ca5acfa76a6
john fawzy <68279241+johnfms@users.noreply.github.com>
2024-08-25 18:02:42 +0300

    Add files via upload

---
+++ ADAS/ADAS/ADAS/main.c
@@ -0,0 +1,15 @@
+/*
+ * ADAS.c
+ * Authors : john , Ritaj , Rahaf , Omar , Farida
+ */
+
+//==--libraries--==
+#include <avr/io.h>
+
+//==--Start Main--==
+int main(void){
+    while (1) {
+		
+    }
+}
+
